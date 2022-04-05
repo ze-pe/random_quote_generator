f9a00edebedcba768ce2725a753f156e96c983bb
ze-pe <83299059+ze-pe@users.noreply.github.com>
2022-04-04 20:36:36 -0400

    group project completed

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log('Connected!');
+
+const url="https://type.fit/api/quotes";
+
+const buttomElement = document.getElementById('quote');
+
+
+
+async function clickButton () {
+    let response = await fetch(url);
+
+    const container = document.getElementById('container');
+
+    if (response.status === 200) {
+         // convert json response to an object
+        const jsonObject = await response.json();
+        // generate random index to select a random object {text:author} within the array
+        let index = Math.floor(Math.random() * jsonObject.length);
+        console.log(jsonObject[index]);
+
+        // const quoteBox = document.createElement('div').innerHTML = jsonObject[index].text;
+        // container.append(quoteBox);
+
+        const quoteBox = jsonObject[index].text;
+        container.innerHTML = quoteBox;
+    }
+};
+
+buttomElement.addEventListener('click',clickButton);
