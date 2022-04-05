775fe8b262e85f2328230d2592a7609cf2427ed9
ze-pe <83299059+ze-pe@users.noreply.github.com>
2022-04-04 20:53:44 -0400

    add author div

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ async function clickButton () {
     let response = await fetch(url);
 
     const container = document.getElementById('container');
+    const author = document.getElementById('author');
 
     if (response.status === 200) {
          // convert json response to an object
@@ -20,6 +21,9 @@ async function clickButton () {
 
         // const quoteBox = document.createElement('div').innerHTML = jsonObject[index].text;
         // container.append(quoteBox);
+
+        const authorBox = jsonObject[index].author;
+        author.innerHTML = authorBox;
 
         const quoteBox = jsonObject[index].text;
         container.innerHTML = quoteBox;
