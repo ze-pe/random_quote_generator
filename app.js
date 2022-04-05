dd46ee5d4d0a523ff703bb0fd7c942afe24d12bc
ze-pe <83299059+ze-pe@users.noreply.github.com>
2022-04-04 21:20:32 -0400

    in progress bonus question

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
-console.log('Connected!');
+// console.log('Connected!');
+
+// const url="https://type.fit/api/quotes";
+
+// const buttomElement = document.getElementById('quote');
+
+// async function clickButton () {
+//     let response = await fetch(url);
+
+//     const container = document.getElementById('container');
+//     const author = document.getElementById('author');
+
+//     if (response.status === 200) {
+//          // convert json response to an object
+//         const jsonObject = await response.json();
+//         // generate random index to select a random object {text:author} within the array
+//         let index = Math.floor(Math.random() * jsonObject.length);
+//         console.log(jsonObject[index]);
+
+//         // const quoteBox = document.createElement('div').innerHTML = jsonObject[index].text;
+//         // container.append(quoteBox);
+
+//         const authorBox = jsonObject[index].author;
+//         author.innerHTML = authorBox;
+
+//         const quoteBox = jsonObject[index].text;
+//         container.innerHTML = quoteBox;
+//     }
+// };
+
+// buttomElement.addEventListener('click',clickButton);
+
+
+
+
 
 const url="https://type.fit/api/quotes";
 
-const buttomElement = document.getElementById('quote');
+const authorButton = document.getElementById('quote-me');
 
-
-
-async function clickButton () {
+async function quoteMeButton () {
     let response = await fetch(url);
 
     const container = document.getElementById('container');
@@ -15,9 +47,17 @@ async function clickButton () {
     if (response.status === 200) {
          // convert json response to an object
         const jsonObject = await response.json();
-        // generate random index to select a random object {text:author} within the array
-        let index = Math.floor(Math.random() * jsonObject.length);
-        console.log(jsonObject[index]);
+        const dropDown = document.getElementById('author-select');
+
+        jsonObject.forEach(obj => {
+            const option = document.createElement('option');
+            option.value = obj.author;
+            option.text = obj.author;
+            console.log(obj.author);
+            console.log(option);
+            //option.innerHTML = obj.author;
+            dropDown.appendChild(option);
+        })
 
         // const quoteBox = document.createElement('div').innerHTML = jsonObject[index].text;
         // container.append(quoteBox);
@@ -28,6 +68,7 @@ async function clickButton () {
         const quoteBox = jsonObject[index].text;
         container.innerHTML = quoteBox;
     }
-};
+}
 
-buttomElement.addEventListener('click',clickButton);
+authorButton.addEventListener('click',quoteMeButton);
+
